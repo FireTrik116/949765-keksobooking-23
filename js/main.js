7416bc81a8379e18a56914b3ac596f7cf56e7dd2
FireTrik116 <vasinokezhka@gmail.com>
2021-05-31 20:17:36 +0300

    Исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function getRandomInteger(min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-getRandomInteger(100, 90);
+  getRandomInteger(100, 50);
 
 function getRandomFloat(min, max, fractionDigits) {
   const fractionMultiplier = Math.pow(10, fractionDigits);
